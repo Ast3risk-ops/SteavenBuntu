af8fe3e743deddbf2dfed73f1a856e0f3cc5df98
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-09-22 22:59:00 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -15,3 +15,5 @@ sudo dpkg --force-all -i ~/pop-shell-theme.deb
 sudo dpkg --force-all -i ~/pop-gtk-theme.deb
 sudo dpkg --force-all -i ~/pop-sound-theme.deb
 sudo apt install -f -y
+gsettings set org.gnome.desktop.interface gtk-theme Pop
+gsettings set org.gnome.desktop.wm.preferences theme Pop
