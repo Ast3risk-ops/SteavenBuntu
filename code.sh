7531859b790ae959b6740eae3da7c19aca403906
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-09-23 00:03:06 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -8,19 +8,21 @@ sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/01-network-man
 sudo add-apt-repository ppa:papirus/papirus -y
 sudo apt update
 sudo apt upgrade -y
-sudo apt install --no-install-recommends gnome -y
+sudo apt-get install xorg xterm menu gksu synaptic --no-install-recommends -y
+sudo apt-get install gnome-session gnome-panel metacity gnome-terminal --no-install-recommends -y
 sudo apt install gnome-shell-extension-dash-to-panel gnome-shell-extension-desktop-icons gnome-shell-extension-gamemode -y
 sudo apt install nemo -y
 sudo apt install lightdm lightdm-settings -y
 sudo apt install git wget -y
 sudo apt install papirus-icon-theme -y
-sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-gnome-shell-theme_5.3.1_all.deb -P ~/pop-shell-theme.deb
-sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-gtk-theme_5.3.1_all.deb -P ~/pop-gtk-theme.deb
-sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-sound-theme_5.3.1_all.deb -P ~/pop-sound-theme.deb
+sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-gnome-shell-theme_5.3.1_all.deb -o ~/pop-shell-theme.deb
+sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-gtk-theme_5.3.1_all.deb -o ~/pop-gtk-theme.deb
+sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/pop-sound-theme_5.3.1_all.deb -o ~/pop-sound-theme.deb
 sudo dpkg --force-all -i ~/pop-shell-theme.deb
 sudo dpkg --force-all -i ~/pop-gtk-theme.deb
 sudo dpkg --force-all -i ~/pop-sound-theme.deb
 sudo apt install -f -y
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
 gsettings set org.gnome.desktop.interface gtk-theme Pop
 gsettings set org.gnome.desktop.wm.preferences theme Pop
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Light
