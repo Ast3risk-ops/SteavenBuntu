fdc84b42644ed89d9869586344cc63b49abc9327
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-09-22 22:52:45 +0200

    a

--- code.sh
+++ code.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo apt purge snapd -y
 sudo wget https://github.com/SteavenGamerYT/SteavenBuntu/raw/main/nosnap.pref -P /etc/apt/preferences.d/nosnap.pref
+sudo add-apt-repository ppa:papirus/papirus -y
 sudo apt update
 sudo apt upgrade -y
 sudo apt install --no-install-recommends gnome -y
 sudo apt install lightdm lightdm-settings -y
 sudo apt install git wget -y
+sudo apt install papirus-icon-theme -y
