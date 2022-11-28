ae7954e8d9094c4f7b9d041c38a15351114fdedc
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-11-29 00:29:17 +0200

    Update Arch.sh

--- Arch.sh
+++ Arch.sh
@@ -7,10 +7,11 @@ cd yay
 makepkg -si
 
 # install gnome and gdm3
-sudo pacman -Syu --noconfirm --needed gnome gdm
+sudo pacman -S --noconfirm --needed gnome gdm
+sudo systemctl enable gdm
 
 #zsh
-sudo pacman -Syu --noconfirm --needed zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+sudo pacman -S --noconfirm --needed zsh zsh-syntax-highlighting autojump zsh-autosuggestions
 touch "$HOME/.cache/zshhistory"
 #-- Setup Alias in $HOME/zsh/aliasrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/,zsh/plugins/powerlevel10k
@@ -33,17 +34,17 @@ gsettings set org.gnome.desktop.interface gtk-theme Nordic
 gsettings set org.gnome.desktop.wm.preferences theme Nordic
 
 # icon theme
-sudo pacman -Syu --noconfirm --needed papirus-icon-theme
+sudo pacman -S --noconfirm --needed papirus-icon-theme
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
 
 # Install brave-browser
-yay -Syu --noconfirm --needed brave-browser
+yay -S --noconfirm --needed brave-bin
 
 # remove firefox
 sudo snap remove firefox
 
 # installing vlc
-sudo pacman -Syu --noconfirm --needed vlc 
+sudo pacman -S --noconfirm --needed vlc 
 
 # make english default
 echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
