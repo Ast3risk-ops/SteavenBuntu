77240720dadbeba7086621210cbc74fb810c1662
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-11-29 18:48:12 +0200

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# remove old bash and zsh and p10k configs
 rm -rf ~/.zshrc
-wget https://github.com/SteavenGamerYT/Gnome-Customizations/raw/main/dotfiles/.zshrc -P ~
 rm -rf ~/.bashrc
-wget https://github.com/SteavenGamerYT/Gnome-Customizations/raw/main/dotfiles/.bashrc -P ~
 rm -rf ~/.p10k.zsh
+
+# download new
+wget https://github.com/SteavenGamerYT/Gnome-Customizations/raw/main/dotfiles/.zshrc -P ~
+wget https://github.com/SteavenGamerYT/Gnome-Customizations/raw/main/dotfiles/.bashrc -P ~
 wget https://github.com/SteavenGamerYT/Gnome-Customizations/raw/main/dotfiles/.p10k.zsh -P ~
