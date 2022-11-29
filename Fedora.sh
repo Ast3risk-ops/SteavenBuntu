1c4371e777ee3bb99e1952f2b90ebd87e0e7435c
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2022-11-29 22:14:15 +0200

    added new zsh plugin

--- Fedora.sh
+++ Fedora.sh
@@ -27,6 +27,8 @@ mkdir ~/.zsh
 mkdir ~/.zsh/plugins/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/plugins/powerlevel10k
 echo 'source ~/.zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/plugins/zsh-autocomplete
+echo 'source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >> ~/.zshrc
 
 # make zsh default
 chsh -s $(which zsh)
--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -6,6 +6,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 source ~/.zsh/plugins/powerlevel10k/powerlevel10k.zsh-theme
+source ~/.zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
